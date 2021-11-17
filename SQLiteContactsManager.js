b272125660aa1c72df1216f88fada3629d4c6c7d
bolatah <bolatah@outlook.com>
2021-11-17 22:08:33 +0100

    first commit

---
+++ SQLiteContactsManager.js
@@ -0,0 +1,107 @@
+const sqlite3 = require('sqlite3');
+require('dotenv').config({path:'/.env'});
+
+const db = new sqlite3.Database("process.env.DB");
+
+module.exports = class SQLiteContactsManager {
+constructor() {
+    this.init();
+}
+init() {
+    db.run(`
+    CREATE TABLE IF NOT EXISTS
+        contacts(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            email TEXT,
+            message TEXT
+        )`
+    );
+}
+async addContact(contact){
+    return new Promise ((resolve, reject) => {
+        db.run(
+            `INSERT
+                INTO contacts(name, email, message)
+                VALUES(?,?,?)`, 
+                [contact.name, contact.email, contact.message],
+                function () {
+                    resolve(this.lastID);
+                }
+        );
+    })
+}
+
+async getContact(id){
+    return new Promise ((resolve, reject) => {
+        db.get(
+            'SELECT * FROM contacts where id=?',
+                [id],
+                (error, row) => {
+                    if(error){
+                        reject(error);
+                    } else {
+                        resolve(row);
+                    }
+                }
+        );
+    })
+}
+
+async updateContact(id , contact){
+    return new Promise ((resolve, reject) => {
+        db.run(
+            `UPDATE contacts SET
+                name = ?,
+                email = ?,
+                message = ?,
+            WHERE id = ?`,
+            [
+                contact.name,
+                contact.email,
+                contact.message,
+                id
+            ],
+            (error, row) => {
+                if(error) {
+                    reject(error);
+                } else {
+                    resolve(row);
+                }
+            }
+        );
+    })
+}
+
+async deleteContact(id){
+    return new Promise((resolve, reject) => {
+        db.run(
+            'DELETE FROM contacts WHERE id = ?',
+            [id],
+            (error, row) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(row);
+                }
+            }
+        );
+    })
+}
+
+async getContacts(){
+    return new Promise ((resolve, reject) => {
+        db.all(
+            'SELECT * FROM contacts',
+                [],
+                (error, rows) => {
+                    if(error){
+                        reject(error);
+                    } else {
+                        resolve(rows);
+                    }
+                }
+        );
+    })
+    }
+}
